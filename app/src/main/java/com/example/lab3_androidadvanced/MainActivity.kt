490eca2decbbdbda25d5a920cda057246789eff0
dieu <nongngocdieu20122002@gmail.com>
2021-09-24 12:26:53 +0700

    Removed Bai2.kt

--- app/src/main/java/com/example/lab3_androidadvanced/MainActivity.kt
+++ app/src/main/java/com/example/lab3_androidadvanced/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import com.example.lab3_androidadvanced.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Họ tên: Nông Ngọc Diệu
+    // MSSV: DieuNNPH15766
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,11 +17,6 @@ class MainActivity : AppCompatActivity() {
         binding.btnBai1.setOnClickListener {
             startActivity(Intent(this, Bai1::class.java))
         }
-
-        binding.btnBai2.setOnClickListener {
-            startActivity(Intent(this, Bai2::class.java))
-        }
-
 
 
     }
